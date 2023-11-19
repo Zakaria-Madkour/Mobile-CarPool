df7cc0d7321c859f34232ce78c82117acdc63264
Zakaria Madkour <82716623+Zakaria-Madkour@users.noreply.github.com>
2023-11-19 20:16:34 +0200

    Creating Buttom Navigation Bar with 4 fragments

---
+++ app/src/main/java/com/example/majortask/MainActivity.java
@@ -0,0 +1,42 @@
+package com.example.majortask;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.os.Bundle;
+
+import com.example.majortask.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+    ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        replaceFragment(new HomeFragment());
+
+        binding.bottomNavigationView.setOnItemSelectedListener(item -> {
+            if (item.getItemId() == R.id.home){
+                replaceFragment(new HomeFragment());
+            } else if (item.getItemId() == R.id.cart) {
+                replaceFragment(new CartFragment());
+            }else if (item.getItemId() == R.id.payment) {
+                replaceFragment(new PaymentFragment());
+            }else if (item.getItemId() == R.id.history) {
+                replaceFragment(new OrderFragment());
+            }
+            return true;
+        });
+    }
+
+    private void replaceFragment(Fragment fragment){
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frame_layout, fragment);
+        fragmentTransaction.commit();
+    }
+}
